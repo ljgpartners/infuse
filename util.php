0d3ab22529b2052ce800f2d1fc95b49971085ebf
Bryan Perez <perezjbryan@gmail.com>
2013-04-12 16:13:36 -0700

    init

---
+++ util.php
@@ -0,0 +1,45 @@
+<?php namespace Bpez\Infuse;
+
+	class Util {
+
+		/**
+		 * 
+		 *
+		 * @param  string  $driver
+		 * @return mixed
+		 */
+		public static function get($name)
+		{
+			return (isset($_POST["{$name}"])? $_POST["{$name}"] : 
+				(isset($_GET["{$name}"]))? $_GET["{$name}"] : false );
+		}
+
+		/**
+		 * .
+		 *
+		 * @param  string  $driver
+		 * @return mixed
+		 */
+		public static function truncateText($text, $nbrChar, $append='...') 
+		{
+			if (strlen($text) > $nbrChar) {
+		  	$text = substr($text, 0, $nbrChar);
+		    $text .= $append;
+		 	}
+		  return $text;
+		}
+
+		/**
+		 * 
+		 *
+		 * @param  string  $driver
+		 * @return mixed
+		 */
+		public static function debug($var)
+		{
+			return "<pre>".print_r($var)."</pre>";
+		}
+
+	}
+
+?>
