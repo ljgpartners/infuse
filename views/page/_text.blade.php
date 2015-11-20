d725f6a6cb8513f5eccb3092f9338fb81907a6e7
Bryan Perez <perezjbryan@gmail.com>
2015-11-20 15:58:19 -0800

    Fix php tag error

--- views/page/_text.blade.php
+++ views/page/_text.blade.php
@@ -1,4 +1,4 @@
-<? 
+<?php
 $extractId = (isset($group))? $group->id : $element->id;
 $extractLabel = (isset($group))? "Group" : "Text";
 $extractPip = (isset($pip))? $pip : "page";
@@ -8,11 +8,11 @@ $extractPip = (isset($pip))? $pip : "page";
 	<div class="col-sm-12 col-xs-12">
 		<div class="input-group">
 			<span class="input-group-addon">{{$element->name}}</span>
-			<textarea class="infusePagesCkeditor form-control infusePageSerialize textareaEditEvent"  
-				data-serialize-tag="text" 
-				data-serialize-page-value="1" 
-				data-serialize-page-value-name="{{$element->name}}" 
-				data-serialize-page-value-description="{{$element->description}}" 
+			<textarea class="infusePagesCkeditor form-control infusePageSerialize textareaEditEvent"
+				data-serialize-tag="text"
+				data-serialize-page-value="1"
+				data-serialize-page-value-name="{{$element->name}}"
+				data-serialize-page-value-description="{{$element->description}}"
 				data-serialize-page-value-id="{{$element->id}}">
 				{{$element->value}}
 			</textarea>
